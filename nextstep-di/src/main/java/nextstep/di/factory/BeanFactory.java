fdefa031f598923615ab6df7506538287bdd8be5
hyunsoojung <jhs695@naver.com>
2019-10-29 18:02:24 +0900

    refactor: 주석 제거

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -38,23 +38,19 @@ public class BeanFactory {
     }
 
     private Object instanticate(Class<?> aClass) throws IllegalAccessException, InvocationTargetException, InstantiationException {
-        // Bean 저장소에 clazz에 해당하는 인스턴스가 이미 존재하면 해당 인스턴스 반환
         if (beans.containsKey(aClass)) {
             return beans.get(aClass);
         }
-        // clazz에 @Inject가 설정되어 있는 생성자를 찾는다. BeanFactoryUtils 활용
+
         Constructor constructor = BeanFactoryUtils.getInjectedConstructor(aClass);
 
-        // @Inject로 설정한 생성자가 없으면 Default 생성자로 인스턴스 생성 후 Bean 저장소에 추가 후 반환
         if (Objects.isNull(constructor)) {
             beans.put(aClass, BeanUtils.instantiateClass(aClass));
             return beans.get(aClass);
         }
 
-        // @Inject로 설정한 생성자가 있으면 찾은 생성자를 활용해 인스턴스 생성 후 Bean 저장소에 추가 후 반환
         List<Object> arguments = new ArrayList<>();
         Class[] parameterTypes = constructor.getParameterTypes();
-
         for (Class clazz : parameterTypes) {
             Class cls = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
             arguments.add(instanticate(cls));
