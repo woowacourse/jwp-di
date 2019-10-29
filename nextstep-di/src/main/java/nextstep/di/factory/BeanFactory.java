3811525cfe27fb04e11d951c83265169342c3214
mrkwon <erased11@gmail.com>
2019-10-29 17:15:04 +0900

    feat: BeanFactory initialize method

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -3,19 +3,22 @@ package nextstep.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +27,44 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> preInstantiateBean : preInstantiateBeans) {
+            scanBean(preInstantiateBean);
+        }
+    }
 
+    private Object scanBean(Class<?> preInstantiateBean) {
+        if (beans.containsKey(preInstantiateBean)) {
+            return beans.get(preInstantiateBean);
+        }
+
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(preInstantiateBean);
+
+        if (Objects.isNull(injectedConstructor)) {
+            Object instance = BeanUtils.instantiateClass(preInstantiateBean);
+            beans.put(preInstantiateBean, instance);
+            logger.debug("bean name : {}, instance : {}", preInstantiateBean, instance);
+            return instance;
+        }
+
+        return putParameterizedObject(preInstantiateBean, injectedConstructor);
+    }
+
+    private Object putParameterizedObject(Class<?> preInstantiateBean, Constructor<?> constructor) {
+        Object[] params = getConstructorParams(constructor);
+        Object instance = BeanUtils.instantiateClass(constructor, params);
+        beans.put(preInstantiateBean, instance);
+        logger.debug("bean name : {}, instance : {}", preInstantiateBean, instance);
+        return instance;
+    }
+
+    private Object[] getConstructorParams(Constructor<?> constructor) {
+        Object[] params = new Object[constructor.getParameterCount()];
+        for (int i = 0; i < params.length; i++) {
+            Class<?> parameterType = constructor.getParameterTypes()[i];
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameterType, preInstantiateBeans);
+            params[i] = scanBean(concreteClass);
+        }
+        return params;
     }
 }
+
