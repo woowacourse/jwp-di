7c065bdca2533bb60a85182b9a546a0e19e37b88
gch01410 <gch01410@naver.com>
2019-10-31 15:05:07 +0900

    feat: DI 기능 구현

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -3,19 +3,23 @@ package nextstep.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +28,25 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> clazz : preInstantiateBeans) {
+            beans.put(clazz, instantiate(clazz));
+        }
+    }
 
+    private Object instantiate(Class<?> clazz) {
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (Objects.isNull(constructor)) {
+            return BeanUtils.instantiateClass(clazz);
+        }
+
+        return BeanUtils.instantiateClass(constructor, getParameters(constructor));
+    }
+
+    private Object[] getParameters(Constructor<?> constructor) {
+        return Arrays.stream(constructor.getParameterTypes())
+                .map(clazz -> BeanFactoryUtils.findConcreteClass(clazz, preInstantiateBeans))
+                .map(this::instantiate)
+                .toArray();
     }
 }
