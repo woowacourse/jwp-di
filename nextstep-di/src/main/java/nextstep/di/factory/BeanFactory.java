72eaccce0af37a228b92cfc6de74ecd2f6bc7128
DaejunBae <dpudpu11@gmail.com>
2019-10-31 14:39:57 +0900

    feat: BeanFactory 빈 생성 구현

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -1,11 +1,18 @@
 package nextstep.di.factory;
 
 import com.google.common.collect.Maps;
+import nextstep.exception.BeanFactoryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -24,6 +31,45 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        preInstanticateBeans.forEach(preInstanticateBean -> {
+            Object bean = createBean(preInstanticateBean);
+            beans.put(preInstanticateBean, bean);
+        });
+    }
 
+    private Object createBean(final Class<?> preInstanticateBean) {
+        Optional<Constructor<?>> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(preInstanticateBean);
+
+        return injectedConstructor.map(this::createInjectedBean)
+                .orElseGet(() -> createConcreteClassBean(preInstanticateBean));
+
+    }
+
+    private Object createInjectedBean(final Constructor<?> injectedConstructor) {
+        try {
+            Class<?>[] parameterTypes = injectedConstructor.getParameterTypes();
+            List<Object> parameters = createParameters(parameterTypes);
+
+            return injectedConstructor.newInstance(parameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            logger.error(e.getMessage());
+            throw new BeanFactoryException(e);
+        }
+    }
+
+    private List<Object> createParameters(final Class<?>[] parameterTypes) {
+        return Stream.of(parameterTypes)
+                .map(this::createBean)
+                .collect(Collectors.toList());
+    }
+
+    private Object createConcreteClassBean(final Class<?> preInstanticateBean) {
+        try {
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(preInstanticateBean, preInstanticateBeans);
+            return concreteClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.error(e.getMessage());
+            throw new BeanFactoryException(e);
+        }
     }
 }
