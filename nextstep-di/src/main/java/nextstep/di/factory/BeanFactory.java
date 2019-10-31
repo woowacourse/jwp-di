752ace619995811a1aab4754d4dbece48985e964
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-31 11:21:27 +0900

    feat: create DI of Service Annotation

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -1,9 +1,12 @@
 package nextstep.di.factory;
 
 import com.google.common.collect.Maps;
+import nextstep.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,6 +27,36 @@ public class BeanFactory {
     }
 
     public void initialize() {
-        
+        for (Class clazz : preInstantiateBeans) {
+            if (clazz.isAnnotationPresent(Service.class)) {
+                Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+                Class<?>[] parameterTypes = injectedConstructor.getParameterTypes();
+
+                Object[] constructors = getParameterInstances(parameterTypes);
+
+                try {
+                    beans.put(clazz, injectedConstructor.newInstance(constructors));
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    private Object[] getParameterInstances(Class<?>[] clazzs) {
+        Object[] constructors = new Object[clazzs.length];
+        for (int i = 0; i < clazzs.length; i++) {
+            Class parameterType = clazzs[i];
+
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameterType, preInstantiateBeans);
+            try {
+                constructors[i] = concreteClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return constructors;
     }
 }
