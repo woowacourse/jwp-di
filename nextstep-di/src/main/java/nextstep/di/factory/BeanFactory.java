4a2c7e9c1c68ea24849cdfc52e5400b05e873f3a
Ko eunsuk <eunsukko.128@gmail.com>
2019-10-31 14:47:14 +0900

    refactor: BeanFactory 의미있는 함수, 변수 명으로 변경

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -4,9 +4,9 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -35,60 +35,47 @@ public class BeanFactory {
             if (beans.containsKey(root)) {
                 continue;
             }
-
-            try {
-                fill(root);
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            }
+            instantiateClass(root);
         }
     }
 
-    // beans
-    private void fill(Class<?> from) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        if (visited.contains(from)) {
-            if (!beans.containsKey(from)) {
+    private void instantiateClass(Class<?> clazz) {
+        if (visited.contains(clazz)) {
+            if (!beans.containsKey(clazz)) {
                 throw new IllegalArgumentException("사이클..!!");
             }
 
-            return ;
+            return;
         }
-        visited.add(from);
+        visited.add(clazz);
 
-        List<Class<?>> toNodes = toNodes(from);
-        for (Class<?> to : toNodes) {
-            fill(to);
+        // concrete
+        Constructor<?> constructor = (BeanFactoryUtils.getInjectedConstructor(clazz) != null) ? BeanFactoryUtils.getInjectedConstructor(clazz) : clazz.getConstructors()[0];
+
+        for (Class<?> parameterType : getParameterTypes(constructor)) {
+            instantiateClass(parameterType);
         }
 
         // 생성자 채우기
-        beans.put(from, from.getConstructors()[0].newInstance(intances(toNodes)));
+        beans.put(clazz, instantiate(constructor));
     }
 
-    private Object[] intances(List<Class<?>> toNodes) {
-        return toNodes.stream()
-                .map(node -> beans.get(node))
-                .collect(Collectors.toList()).toArray();
+    private List<Class<?>> getParameterTypes(Constructor<?> constructor) {
+        return findConcreteClass(Arrays.asList(constructor.getParameterTypes()));
     }
 
-    // from 이란 애를 받으면
-    // @Injects 가 달린 생성자에서 요구하는 클래스들을 리턴
-    // 없으면 빈 리스트 리턴
-    public List<Class<?>> toNodes(Class<?> from) {
-        Constructor<?> constructor =  BeanFactoryUtils.getInjectedConstructor(from);
-        if (constructor == null) {
-            return Collections.emptyList();
-        }
-
-        // 파라미터 인터페이스가 존재할 수 있음
-        // 등록된 구현체를 넣어주어야?
-        return Arrays.asList(constructor.getParameterTypes()).stream()
+    private List<Class<?>> findConcreteClass(List<Class<?>> classes) {
+        return classes.stream()
                 .map(parameter -> BeanFactoryUtils.findConcreteClass(parameter, preInstanticateBeans))
                 .collect(Collectors.toList());
+    }
+
+    private Object instantiate(Constructor<?> constructor) {
+        return BeanUtils.instantiateClass(constructor, getBeans(getParameterTypes(constructor)));
+    }
+
+    private Object[] getBeans(List<Class<?>> parameterTypes) {
+        return parameterTypes.stream()
+                .map(node -> beans.get(node)).toArray();
     }
 }
