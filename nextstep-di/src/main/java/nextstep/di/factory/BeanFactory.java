f9d30d74183d5dd54f694ff470d6b0ff441ec358
heebong <heebg2@naver.com>
2019-10-31 13:15:31 +0900

    feat: Bean Factory 구현
    - `@Injected` Annotation 찾아서 DI 적용(빈 생성)

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -3,19 +3,23 @@ package nextstep.di.factory;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +28,31 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> clazz : preInstantiateBeans) {
+            beans.put(clazz, createBean(clazz));
+        }
+    }
 
+    private Object createBean(Class<?> clazz) {
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        if (injectedConstructor == null) {
+            return getInstance(clazz);
+        }
+        Class[] constructorParameterTypes = injectedConstructor.getParameterTypes();
+        Object[] constructorParameterInstance = Arrays.stream(constructorParameterTypes)
+            .map(injected -> BeanFactoryUtils.findConcreteClass(injected, preInstantiateBeans))
+            .map(this::createBean)
+            .toArray();
+
+        return BeanUtils.instantiateClass(injectedConstructor, constructorParameterInstance);
+    }
+
+    private Object getInstance(Class<?> clazz) {
+        try {
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            logger.error("Error", e);
+            throw new InstantiateBeanException(e);
+        }
     }
 }
---
+++ nextstep-di/src/main/java/nextstep/di/factory/InstantiateBeanException.java
@@ -0,0 +1,7 @@
+package nextstep.di.factory;
+
+public class InstantiateBeanException extends RuntimeException {
+    public InstantiateBeanException(Throwable e) {
+        super(e);
+    }
+}
