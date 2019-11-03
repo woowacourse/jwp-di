5700f216340bc861de2b2088f73ce55789febdf6
gch01410 <gch01410@naver.com>
2019-11-03 14:20:06 +0900

    test: BeanFactory, BeanFactoryUtils 테스트 구현

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -1,20 +1,27 @@
 package nextstep.di.factory;
 
 import com.google.common.collect.Sets;
+import nextstep.annotation.Inject;
+import nextstep.di.factory.example.MyQnaService;
 import nextstep.di.factory.example.QnaController;
+import nextstep.exception.BeanInstantiationException;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
-import nextstep.di.factory.example.MyQnaService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
@@ -30,6 +37,37 @@ public class BeanFactoryTest {
         Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
         beanFactory = new BeanFactory(preInstanticateClazz);
         beanFactory.initialize();
+    }
+
+    @DisplayName("Bean 초기화 확인")
+    @Test
+    void initialize() {
+        Set<Class<?>> preInstantiateBeans = new HashSet<>(Arrays.asList(TestClass.class));
+        BeanFactory beanFactory = new BeanFactory(preInstantiateBeans);
+        beanFactory.initialize();
+
+        assertThat(beanFactory.getBean(TestClass.class)).isInstanceOf(TestClass.class);
+    }
+
+    @DisplayName("Bean 초기화 실패 확인")
+    @Test
+    void failInitialize() {
+        Set<Class<?>> preInstantiateBeans = new HashSet<>(Arrays.asList(TestClass.class, TestClassWithoutInject.class));
+        BeanFactory beanFactory = new BeanFactory(preInstantiateBeans);
+
+        assertThatExceptionOfType(BeanInstantiationException.class)
+                .isThrownBy(beanFactory::initialize);
+    }
+
+    @DisplayName("Controller 클래스 반환 확인")
+    @Test
+    void getController() {
+        Set<Class<?>> preInstantiateBeans = new HashSet<>(Arrays.asList(TestClass.class, TestClassWithoutController.class));
+        BeanFactory beanFactory = new BeanFactory(preInstantiateBeans);
+        beanFactory.initialize();
+
+        assertThat(beanFactory.getController()).contains(TestClass.class);
+        assertThat(beanFactory.getController()).doesNotContain(TestClassWithoutController.class);
     }
 
     @Test
@@ -52,5 +90,22 @@ public class BeanFactoryTest {
         }
         log.debug("Scan Beans Type : {}", beans);
         return beans;
+    }
+
+    @Controller
+    private class TestClass {
+        @Inject
+        public TestClass() {
+        }
+    }
+
+    @Controller
+    private class TestClassWithoutInject {
+    }
+
+    private class TestClassWithoutController {
+        @Inject
+        public TestClassWithoutController() {
+        }
     }
 }
