4bc2d6e7207e291a32da549b428f83105ce336d3
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-11-04 03:07:37 +0900

    debug: fix error that was caused by checkCycleReference

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -29,16 +29,19 @@ public class BeanFactory {
     }
 
     public void initialize() {
-        checkCycleReference(preInstantiateBeans);
+        Set<Class<?>> concretePreInstantiateBeans = preInstantiateBeans.stream()
+                .map(preinstantiateBean -> BeanFactoryUtils.findConcreteClass(preinstantiateBean, preInstantiateBeans))
+                .collect(Collectors.toSet());
+        checkCycleReference(concretePreInstantiateBeans);
         preInstantiateBeans.forEach(this::createBeanWithTryCatch);
     }
 
     private void checkCycleReference(Set<Class<?>> preInstantiateBeans) {
-        List<String> beanNames = new ArrayList<>();
-        preInstantiateBeans.forEach(beanClass -> checkCycleReference(beanClass, beanNames));
+        preInstantiateBeans.forEach(beanClass -> checkCycleReference(beanClass, new ArrayList<>()));
     }
 
     private void checkCycleReference(Class<?> beanClass, List<String> beanNames) {
+        beanClass = BeanFactoryUtils.findConcreteClass(beanClass, preInstantiateBeans);
         Constructor<?> constructor = getConstructor(beanClass);
         if (beanNames.contains(constructor.getName())) {
             throw new CycleReferenceException();
@@ -57,7 +60,7 @@ public class BeanFactory {
 
     private Object createBean(Class<?> beanClass) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
         if (beans.keySet().contains(beanClass)) {
-            return null;
+            return beans.get(beanClass);
         }
         beanClass = BeanFactoryUtils.findConcreteClass(beanClass, preInstantiateBeans);
         Constructor<?> constructor = getConstructor(beanClass);
