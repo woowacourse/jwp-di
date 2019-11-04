2898d58eb716f4843faaf2c734f57c4e37011b5d
yk1028 <wongeunsong94@gmail.com>
2019-11-04 17:05:58 +0900

    feat: BeanFactory 생성자 찾는 기능 추가

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactory.java
@@ -1,11 +1,17 @@
 package nextstep.di.factory;
 
 import com.google.common.collect.Maps;
+import nextstep.annotation.Inject;
+import nextstep.exception.NotFoundConstructorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -24,6 +30,27 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        logger.debug("Initialize BeanFactory!");
+        preInstanticateBeans.forEach(bean -> {
+            logger.debug(bean.getName());
+            logger.debug(findConstructor(bean).getName());
+        });
+    }
 
+    private Constructor findConstructor(Class<?> bean) {
+        Constructor<?>[] constructors = bean.getConstructors();
+        if (constructors.length == 1) {
+            return constructors[0];
+        }
+
+        List<Constructor> list = Arrays.stream(bean.getConstructors())
+                .filter(constructor -> constructor.isAnnotationPresent(Inject.class))
+                .collect(Collectors.toList());
+
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+
+        throw new NotFoundConstructorException("올바른 생성자를 찾을 수 없습니다.");
     }
 }
