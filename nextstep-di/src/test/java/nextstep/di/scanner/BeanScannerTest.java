c89e5bca0ff02396c701792411acfbf77c999958
mrkwon <erased11@gmail.com>
2019-11-06 02:31:46 +0900

    style: change method name to display name annotation

--- nextstep-di/src/test/java/nextstep/di/scanner/BeanScannerTest.java
+++ nextstep-di/src/test/java/nextstep/di/scanner/BeanScannerTest.java
@@ -3,6 +3,7 @@ package nextstep.di.scanner;
 import nextstep.di.factory.example.JdbcQuestionRepository;
 import nextstep.di.factory.example.JdbcUserRepository;
 import nextstep.di.factory.example.QnaController;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,27 +11,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BeanScannerTest {
 
+    public static final String TEST_BASE_PACKAGE = "nextstep.di.factory.example";
+
     @Test
-    void beanScanner가_모든_Bean을_정상적으로_스캔한다() {
-        BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
+    @DisplayName("beanScanner가 모든 Bean을 정상적으로 스캔한다.")
+    void scanBeanSuccessfully() {
+        BeanScanner beanScanner = new BeanScanner(TEST_BASE_PACKAGE);
         assertEquals(beanScanner.getBeans().size(), 4);
     }
 
     @Test
-    void Controller_어노테이션_클래스를_정상적으로_스캔한다() {
-        BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
+    @DisplayName("Controller 어노테이션 클래스를 정상적으로 스캔한다.")
+    void scanControllersSuccessfully() {
+        BeanScanner beanScanner = new BeanScanner(TEST_BASE_PACKAGE);
         assertTrue(beanScanner.getBeans().contains(QnaController.class));
     }
 
     @Test
-    void Service_어노테이션_클래스를_정상적으로_스캔한다() {
-        BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
+    @DisplayName("Service 어노테이션 클래스를 정상적으로 스캔한다.")
+    void scanServicesSuccessfully() {
+        BeanScanner beanScanner = new BeanScanner(TEST_BASE_PACKAGE);
         assertTrue(beanScanner.getBeans().contains(QnaController.class));
     }
 
     @Test
-    void Repository_어노테이션_클래스를_정상적으로_스캔한다() {
-        BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
+    @DisplayName("Repository 어노테이션 클래스를 정상적으로 스캔한다.")
+    void scanRepositoriesSuccessfully() {
+        BeanScanner beanScanner = new BeanScanner(TEST_BASE_PACKAGE);
         assertTrue(beanScanner.getBeans().contains(JdbcQuestionRepository.class));
         assertTrue(beanScanner.getBeans().contains(JdbcUserRepository.class));
     }
