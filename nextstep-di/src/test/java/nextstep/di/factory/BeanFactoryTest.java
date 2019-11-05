32521fab038d1179a27a40c47f132b5bd876b7fd
imkimheejoo <jujudamdadi@naver.com>
2019-11-05 16:17:00 +0900

    test: getControllers, getBean 테스트 추가

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -2,6 +2,8 @@ package nextstep.di.factory;
 
 import nextstep.di.factory.example.MyQnaService;
 import nextstep.di.factory.example.QnaController;
+import nextstep.di.factory.example.QuestionRepository;
+import nextstep.di.factory.example.UserRepository;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
@@ -11,11 +13,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BeanFactoryTest {
-    private static final Logger log = LoggerFactory.getLogger( BeanFactoryTest.class );
+    private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
 
     private BeanFactory beanFactory;
 
@@ -39,4 +43,25 @@ public class BeanFactoryTest {
         assertNotNull(qnaService.getUserRepository());
         assertNotNull(qnaService.getQuestionRepository());
     }
+
+    @Test
+    public void getControllers() throws Exception {
+        Map<Class<?>, Object> beanByAnnotation = beanFactory.getBeanByAnnotation(Controller.class);
+
+        assertNotNull(beanByAnnotation.get(QnaController.class));
+    }
+
+    @Test
+    public void getBean() throws Exception {
+        Object bean1 = beanFactory.getBean(MyQnaService.class).getUserRepository();
+        Object bean2 = beanFactory.getBean(UserRepository.class);
+
+        Object bean3 = beanFactory.getBean(MyQnaService.class).getQuestionRepository();
+        Object bean4 = beanFactory.getBean(QuestionRepository.class);
+
+        assertThat(bean1).isEqualTo(bean2);
+        assertThat(bean3).isEqualTo(bean4);
+    }
+
+
 }
