9461eb457774399c696813f0ba22848904e2d3b8
kangmin46 <kangmin789@naver.com>
2019-11-05 15:14:36 +0900

    refactor : code formatting

--- nextstep-di/src/main/java/nextstep/exception/BeanCreateFailException.java
+++ nextstep-di/src/main/java/nextstep/exception/BeanCreateFailException.java
@@ -1,7 +1,8 @@
 package nextstep.exception;
 
-public class BeanCreateFailException extends RuntimeException{
+public class BeanCreateFailException extends RuntimeException {
     private static final String BEAN_CREATE_FAIL_MESSAGE = "빈 생성에 실패했습니다.";
+
     public BeanCreateFailException() {
         super(BEAN_CREATE_FAIL_MESSAGE);
     }
