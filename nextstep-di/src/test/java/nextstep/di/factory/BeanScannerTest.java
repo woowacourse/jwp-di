9ea6ed862c82d70d1bc83e1f3c294d81ae8d22a8
pkch93 <star_final@naver.com>
2019-11-05 16:57:58 +0900

    test: BeanScanner 테스트케이스 추가

--- nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
@@ -1,9 +1,13 @@
 package nextstep.di.factory;
 
+import nextstep.di.factory.example.TestApplication;
 import nextstep.di.factory.example.controller.QnaController;
 import nextstep.di.factory.example.repository.JdbcQuestionRepository;
 import nextstep.di.factory.example.repository.JdbcUserRepository;
 import nextstep.di.factory.example.service.MyQnaService;
+import nextstep.di.factory.outside.OutsideController;
+import nextstep.di.factory.outside.OutsideRepository;
+import nextstep.di.factory.outside.OutsideService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,9 +22,32 @@ public class BeanScannerTest {
     void scanAll() {
         Set<Class<?>> preInstantiateClazz = BeanScanner.scan("nextstep.di.factory.example");
 
-        assertThat(preInstantiateClazz).contains(QnaController.class, MyQnaService.class, JdbcQuestionRepository.class, JdbcUserRepository.class);
+        assertThat(preInstantiateClazz).contains(
+                QnaController.class,
+                MyQnaService.class,
+                JdbcQuestionRepository.class,
+                JdbcUserRepository.class
+        );
         assertThat(preInstantiateClazz.size()).isEqualTo(4);
     }
 
+    @Test
+    @DisplayName("scan 메서드에 해당하는 패키지와 하위 패키지 이외의 클래스는 스캔하지 않는다.")
+    void notScanOutsideOfPackage() {
+        Set<Class<?>> preInstantiateClazz = BeanScanner.scan("nextstep.di.factory.example");
 
+        assertThat(preInstantiateClazz).doesNotContain(
+                OutsideController.class,
+                OutsideService.class,
+                OutsideRepository.class
+        );
+    }
+
+    @Test
+    @DisplayName("@Controller, @Service, @Repository 이외의 애노테이션이 선언된 클래스는 스캔하지 않는다.")
+    void ignoreInvalidAnnotation() {
+        Set<Class<?>> preInstantiateClazz = BeanScanner.scan("nextstep.di.factory.example");
+
+        assertThat(preInstantiateClazz).doesNotContain(TestApplication.class);
+    }
 }
---
+++ nextstep-di/src/test/java/nextstep/di/factory/outside/OutsideRepository.java
@@ -0,0 +1,7 @@
+package nextstep.di.factory.outside;
+
+import nextstep.stereotype.Repository;
+
+@Repository
+public class OutsideRepository {
+}
