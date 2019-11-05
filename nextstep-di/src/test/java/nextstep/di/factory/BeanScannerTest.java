523fab6aabe27b3ba4ba8c2e53b646781c8a7e1e
pkch93 <star_final@naver.com>
2019-11-05 16:35:40 +0900

    refactor: BeanScannerTest의 scanAll 메서드 DisplayName 추가

--- nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
@@ -13,15 +13,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BeanScannerTest {
 
-    private BeanScanner beanScanner;
-
     @Test
-    void name() {
-        beanScanner = new BeanScanner();
-
-        Set<Class<?>> preInstantiateClazz = beanScanner.scan("nextstep.di.factory.example");
+    @DisplayName("scan메서드에 패키지명을 넣어주면 해당 패키지의 @Controller, @Service, @Repository 구현 클래스를 스캔한다.")
+    void scanAll() {
+        Set<Class<?>> preInstantiateClazz = BeanScanner.scan("nextstep.di.factory.example");
 
         assertThat(preInstantiateClazz).contains(QnaController.class, MyQnaService.class, JdbcQuestionRepository.class, JdbcUserRepository.class);
         assertThat(preInstantiateClazz.size()).isEqualTo(4);
     }
+
+
 }
