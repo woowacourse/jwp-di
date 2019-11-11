1a96dfecf6f72b3706f0a4612b94b11d0317cda7
hyojaekim <hj00686@naver.com>
2019-11-11 19:12:31 +0900

    feat: BeanDefinition 추상 클래스 생성

---
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanDefinition.java
@@ -0,0 +1,13 @@
+package nextstep.di.factory;
+
+public abstract class BeanDefinition {
+    private Class<?> beanClass;
+
+    public BeanDefinition(Class<?> beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public Class<?> getBeanClass() {
+        return beanClass;
+    }
+}
