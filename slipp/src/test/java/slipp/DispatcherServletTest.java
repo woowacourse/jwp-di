14df6f38828ca0c4d996a715c9e2ae4a0ce7bf22
DaejunBae <dpudpu11@gmail.com>
2019-11-12 02:48:43 +0900

    test: slipp 모듈 DispatcherServletTest di-2단계 적용

--- slipp/src/test/java/slipp/DispatcherServletTest.java
+++ slipp/src/test/java/slipp/DispatcherServletTest.java
@@ -1,5 +1,6 @@
 package slipp;
 
+import nextstep.di.factory.ApplicationContext;
 import nextstep.jdbc.ConnectionManager;
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.asis.ControllerHandlerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import slipp.controller.UserSessionUtils;
 import slipp.domain.User;
+import slipp.support.config.MyConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,8 +30,10 @@ class DispatcherServletTest {
         populator.addScript(new ClassPathResource("jwp.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
 
+        ApplicationContext applicationContext = new ApplicationContext(MyConfiguration.class);
+
         dispatcher = new DispatcherServlet();
-        dispatcher.addHandlerMpping(new AnnotationHandlerMapping("slipp.controller"));
+        dispatcher.addHandlerMpping(new AnnotationHandlerMapping(applicationContext));
 
         dispatcher.addHandlerAdapter(new HandlerExecutionHandlerAdapter());
         dispatcher.addHandlerAdapter(new ControllerHandlerAdapter());
