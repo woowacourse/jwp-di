8aaef73253fd61e926605a82ade6fc9317a7478a
soojinroh <rsj3112@gmail.com>
2019-11-11 14:49:49 +0900

    test: BeanFactory test에서 BeanScanner에 대한 의존성 제거

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -1,14 +1,25 @@
 package nextstep.di.factory;
 
-import nextstep.di.factory.example.JdbcQuestionRepository;
-import nextstep.di.factory.example.MyQnaService;
-import nextstep.di.factory.example.QnaController;
-import nextstep.di.factory.example.QuestionRepository;
+import com.google.common.collect.Sets;
+import nextstep.di.factory.example.repository.JdbcQuestionRepository;
+import nextstep.di.factory.example.service.MyQnaService;
+import nextstep.di.factory.example.controller.QnaController;
+import nextstep.di.factory.example.repository.QuestionRepository;
+import nextstep.di.factory.example2.TestService;
+import nextstep.di.factory.example2.TestServiceObject;
+import nextstep.stereotype.Controller;
+import nextstep.stereotype.Repository;
+import nextstep.stereotype.Service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,12 +28,14 @@ public class BeanFactoryTest {
     private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
 
     private BeanFactory beanFactory;
+    private Reflections reflections;
 
     @BeforeEach
     @SuppressWarnings("unchecked")
     public void setup() {
-        BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
-        beanFactory = new BeanFactory(beanScanner.getPreInstantiateClazz());
+        reflections = new Reflections("nextstep.di.factory.example.");
+        Set<Class<?>> preInstantiateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        beanFactory = new BeanFactory(preInstantiateClazz);
     }
 
     @Test
@@ -53,4 +66,15 @@ public class BeanFactoryTest {
         assertThat(beanFactory.getBean(TestService.class)).isNull();
         assertThat(beanFactory.getBean(TestServiceObject.class)).isNull();
     }
+
+    @SafeVarargs
+    private final Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
+        Set<Class<?>> beans = Sets.newHashSet();
+        for (Class<? extends Annotation> annotation : annotations) {
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        log.debug("Scan Beans Type : {}", beans);
+        return beans;
+    }
+
 }
