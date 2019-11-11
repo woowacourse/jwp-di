0688cdc3f5ecd986045d24416f666e33dd02dd45
starkim06 <starkim06@naver.com>
2019-11-11 14:21:40 +0900

    test: Reflections를 사용하지 않고 테스트 하도록 수정

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -1,22 +1,23 @@
 package nextstep.di.factory;
 
 import com.google.common.collect.Sets;
-import nextstep.di.factory.example.JdbcQuestionRepository;
-import nextstep.di.factory.example.JdbcUserRepository;
-import nextstep.di.factory.example.MyQnaService;
-import nextstep.di.factory.example.QnaController;
+import nextstep.di.factory.circular.CircularDependenceA;
+import nextstep.di.factory.circular.CircularDependenceB;
+import nextstep.di.factory.constructor.AnyService;
+import nextstep.di.factory.constructor.NoDefaultConstructor;
+import nextstep.di.factory.example.*;
+import nextstep.di.factory.notbean.InjectNotBean;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -25,15 +26,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BeanFactoryTest {
     private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
 
-    private static final String NEXTSTEP_DI_FACTORY_EXAMPLE = "nextstep.di.factory.example";
-    private static final String NEXTSTEP_DI_FACTORY_CIRCULAR = "nextstep.di.factory.circular";
-    private static final String NEXTSTEP_DI_FACTORY_CONSTRUCTOR = "nextstep.di.factory.constructor";
-    private static final String NEXTSTEP_DI_FACTORY_NOT_BEAN = "nextstep.di.factory.nobean";
-
+    private static final List<Class<?>> NOT_BEAN_PACKAGE_BEANS = Arrays.asList(InjectNotBean.class);
+    private static final List<Class<?>> CIRCULARDEPENDENCY_PACKAGE_BEANS = Arrays.asList(CircularDependenceA.class, CircularDependenceB.class);
+    private static final List<Class<?>> CONSTRUCTOR_PACKAGE_BEANS = Arrays.asList(AnyService.class, NoDefaultConstructor.class);
+    private static final List<Class<?>> EXAMPLE_PACKAGE_BEANS = Arrays.asList(JdbcQuestionRepository.class, JdbcUserRepository.class, MyQnaService.class, QnaController.class, QuestionRepository.class
+            , UserRepository.class);
 
     @Test
     public void di() {
-        QnaController qnaController = getBeanFactory(NEXTSTEP_DI_FACTORY_EXAMPLE).getBean(QnaController.class);
+        QnaController qnaController = getBeanFactory(EXAMPLE_PACKAGE_BEANS).getBean(QnaController.class);
 
         assertNotNull(qnaController);
         assertNotNull(qnaController.getQnaService());
@@ -46,7 +47,7 @@ public class BeanFactoryTest {
     @Test
     @DisplayName("Controller 가져오기 테스트")
     void getBeansAnnotatedWith1() {
-        Map<Class<?>, Object> controllerBeans = getBeanFactory(NEXTSTEP_DI_FACTORY_EXAMPLE).getBeansAnnotatedWith(Controller.class);
+        Map<Class<?>, Object> controllerBeans = getBeanFactory(EXAMPLE_PACKAGE_BEANS).getBeansAnnotatedWith(Controller.class);
 
         assertThat(controllerBeans.size()).isEqualTo(1);
         assertThat(controllerBeans.get(QnaController.class)).isNotNull();
@@ -55,7 +56,7 @@ public class BeanFactoryTest {
     @Test
     @DisplayName("Service 가져오기 테스트")
     void getBeansAnnotatedWith2() {
-        Map<Class<?>, Object> serviceBeans = getBeanFactory(NEXTSTEP_DI_FACTORY_EXAMPLE).getBeansAnnotatedWith(Service.class);
+        Map<Class<?>, Object> serviceBeans = getBeanFactory(EXAMPLE_PACKAGE_BEANS).getBeansAnnotatedWith(Service.class);
 
         assertThat(serviceBeans.size()).isEqualTo(1);
         assertThat(serviceBeans.get(MyQnaService.class)).isNotNull();
@@ -64,7 +65,7 @@ public class BeanFactoryTest {
     @Test
     @DisplayName("Repository 가져오기 테스트")
     void getBeansAnnotatedWith3() {
-        Map<Class<?>, Object> repositoryBeans = getBeanFactory(NEXTSTEP_DI_FACTORY_EXAMPLE).getBeansAnnotatedWith(Repository.class);
+        Map<Class<?>, Object> repositoryBeans = getBeanFactory(EXAMPLE_PACKAGE_BEANS).getBeansAnnotatedWith(Repository.class);
 
         assertThat(repositoryBeans.size()).isEqualTo(2);
         assertThat(repositoryBeans.get(JdbcQuestionRepository.class)).isNotNull();
@@ -74,14 +75,14 @@ public class BeanFactoryTest {
     @Test
     @DisplayName("순환참조 테스트")
     void circularReference() {
-        assertThrows(IllegalStateException.class, () -> getBeanFactory(NEXTSTEP_DI_FACTORY_CIRCULAR));
+        assertThrows(IllegalStateException.class, () -> getBeanFactory(CIRCULARDEPENDENCY_PACKAGE_BEANS));
 
     }
 
     @Test
     @DisplayName("빈 싱글톤 테스트")
     void singletonBean() {
-        BeanFactory beanFactory = getBeanFactory(NEXTSTEP_DI_FACTORY_EXAMPLE);
+        BeanFactory beanFactory = getBeanFactory(EXAMPLE_PACKAGE_BEANS);
         Object bean1 = beanFactory.getBean(JdbcUserRepository.class);
         Object bean2 = beanFactory.getBean(JdbcQuestionRepository.class);
         Object bean3 = beanFactory.getBean(MyQnaService.class).getUserRepository();
@@ -94,40 +95,27 @@ public class BeanFactoryTest {
     @Test
     @DisplayName("기본 생성자가 존재하지 않고 Inject 어노테이션도 없을 경우 예외 테스트")
     void noDefaultConstructor() {
-        assertThrows(IllegalStateException.class, () -> getBeanFactory(NEXTSTEP_DI_FACTORY_CONSTRUCTOR));
+        assertThrows(IllegalStateException.class, () -> getBeanFactory(CONSTRUCTOR_PACKAGE_BEANS));
     }
 
     @Test
     @DisplayName("빈이 아닌걸 Inject 주입 테스트")
     void injectNotBean() {
-        assertThrows(IllegalStateException.class, () -> getBeanFactory(NEXTSTEP_DI_FACTORY_NOT_BEAN));
+        assertThrows(IllegalStateException.class, () -> getBeanFactory(NOT_BEAN_PACKAGE_BEANS));
     }
 
     @Test
-    @DisplayName("다른 패키지 빈 생성 테스트")
+    @DisplayName("다른 패키지 빈 생성 테스")
     void getOtherPackageBeans() {
-        BeanFactory beanFactory = getBeanFactory(NEXTSTEP_DI_FACTORY_NOT_BEAN);
+        BeanFactory beanFactory = getBeanFactory(EXAMPLE_PACKAGE_BEANS);
         beanFactory.initialize();
-        assertThat(beanFactory.getBean(MyQnaService.class)).isNull();
+        assertThat(beanFactory.getBean(AnyService.class)).isNull();
     }
 
-    @SuppressWarnings("unchecked")
-    public BeanFactory getBeanFactory(String prefix) {
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(prefix, Controller.class, Service.class, Repository.class);
-        BeanFactory beanFactory = new BeanFactory(preInstanticateClazz);
+    public BeanFactory getBeanFactory(List<Class<?>> classes) {
+        BeanFactory beanFactory = new BeanFactory(Sets.newHashSet(classes));
         beanFactory.initialize();
 
         return beanFactory;
-    }
-
-    @SuppressWarnings("unchecked")
-    private Set<Class<?>> getTypesAnnotatedWith(String prefix, Class<? extends Annotation>... annotations) {
-        Reflections reflections = new Reflections(prefix);
-        Set<Class<?>> beans = Sets.newHashSet();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        log.debug("Scan Beans Type : {}", beans);
-        return beans;
     }
 }
