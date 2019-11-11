39dc27c07aa1deeaefe9ae64f4d16feb84b13ab4
김성훈 <seonghun127@gmail.com>
2019-11-11 22:03:41 +0900

    refactor: remove unused class

--- nextstep-di/src/main/java/nextstep/di/factory/exception/UninitializedBeanFactoryException.java
+++
@@ -1,9 +0,0 @@
-package nextstep.di.factory.exception;
-
-public class UninitializedBeanFactoryException extends RuntimeException {
-    private static final String ERROR_MSG = "초기화되지 않은 빈 팩토리에 접근할 수 없습니다.";
-
-    public UninitializedBeanFactoryException() {
-        super(ERROR_MSG);
-    }
-}
