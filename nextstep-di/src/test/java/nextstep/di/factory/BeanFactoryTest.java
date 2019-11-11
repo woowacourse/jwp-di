6bd3f1cbf585f4fa9c94272a1acf1d7905de6506
yeji <yjll1019@naver.com>
2019-11-11 22:11:51 +0900

    test: remove duplicated code

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -1,19 +1,14 @@
 package nextstep.di.factory;
 
-import com.google.common.collect.Sets;
+import java.util.Set;
+
 import nextstep.di.factory.example.MyQnaService;
 import nextstep.di.factory.example.QnaController;
-import nextstep.stereotype.Controller;
-import nextstep.stereotype.Repository;
-import nextstep.stereotype.Service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.annotation.Annotation;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -26,8 +21,10 @@ public class BeanFactoryTest {
     @BeforeEach
     @SuppressWarnings("unchecked")
     public void setup() {
-        reflections = new Reflections("nextstep.di.factory.example");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        final String basePackage = "nextstep.di.factory.example";
+        reflections = new Reflections(basePackage);
+        BeanScanner beanScanner = new BeanScanner(basePackage);
+        Set<Class<?>> preInstanticateClazz = beanScanner.getPreInstanticateClass();
         beanFactory = new BeanFactory(preInstanticateClazz);
         beanFactory.initialize();
     }
@@ -42,15 +39,5 @@ public class BeanFactoryTest {
         MyQnaService qnaService = qnaController.getQnaService();
         assertNotNull(qnaService.getUserRepository());
         assertNotNull(qnaService.getQuestionRepository());
-    }
-
-    @SuppressWarnings("unchecked")
-    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Set<Class<?>> beans = Sets.newHashSet();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        log.debug("Scan Beans Type : {}", beans);
-        return beans;
     }
 }
