45faf26cd031cf7ca5905913d8052d8bb10bdd74
soojinroh <rsj3112@gmail.com>
2019-11-12 19:48:29 +0900

    test: test 코드 수정
    두 set을 비교하는 과정에서 containsAll을 사용하던 것에서 equals로 변경

--- nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanScannerTest.java
@@ -4,6 +4,7 @@ import nextstep.di.factory.example.controller.QnaController;
 import nextstep.di.factory.example.repository.JdbcQuestionRepository;
 import nextstep.di.factory.example.repository.JdbcUserRepository;
 import nextstep.di.factory.example.service.MyQnaService;
+import nextstep.di.factory.example.service.TestService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BeanScannerTest {
     private BeanScanner beanScanner;
@@ -26,8 +27,8 @@ class BeanScannerTest {
     @DisplayName("주입 받은 basePackage에서 해당되는 bean을 스캔하는지 테스트")
     void beanScanTest() {
         Set<Class<?>> actual = beanScanner.getPreInstantiateClazz();
-        Set<Class<?>> expected = new HashSet<>(Arrays.asList(MyQnaService.class, JdbcUserRepository.class, JdbcQuestionRepository.class, QnaController.class));
+        Set<Class<?>> expected = new HashSet<>(Arrays.asList(MyQnaService.class, TestService.class, JdbcUserRepository.class, JdbcQuestionRepository.class, QnaController.class));
 
-        assertTrue(actual.containsAll(expected));
+        assertEquals(actual, expected);
     }
 }
