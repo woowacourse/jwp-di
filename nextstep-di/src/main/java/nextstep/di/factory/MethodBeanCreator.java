aaf2510a03943d44e529981cc01673ce45995b05
green4469 <green4469@gmail.com>
2019-11-13 17:11:40 +0900

    feat: add MethodBeanCreator

---
+++ nextstep-di/src/main/java/nextstep/di/factory/MethodBeanCreator.java
@@ -0,0 +1,31 @@
+package nextstep.di.factory;
+
+import com.google.common.collect.Lists;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class MethodBeanCreator implements BeanCreator {
+    private final Object instance;
+    private final Method method;
+
+    public MethodBeanCreator(Object instance, Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
+    @Override
+    public List<Class<?>> getParams() {
+        return Lists.newArrayList(method.getParameterTypes());
+    }
+
+    @Override
+    public Object instantiate(Object... params) {
+        try {
+            return method.invoke(instance, params);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new ObjectInstantiationFailException(e);
+        }
+    }
+}
