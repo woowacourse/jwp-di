12fcccb4c8983da27d9831ca25d5ef1e08b42e04
kangmin46 <kangmin789@naver.com>
2019-11-13 17:05:15 +0900

    refactor : add another_package_class method

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -3,10 +3,12 @@ package nextstep.di.factory;
 import com.google.common.collect.Sets;
 import nextstep.di.factory.example.MyQnaService;
 import nextstep.di.factory.example.QnaController;
+import nextstep.exception.BeanNotFoundException;
+import nextstep.exception.CircularReferenceException;
+import nextstep.exception.ParameterIsNotBeanException;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
@@ -14,12 +16,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BeanFactoryTest {
     private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
@@ -27,18 +28,14 @@ public class BeanFactoryTest {
     private Reflections reflections;
     private BeanFactory beanFactory;
 
-    @BeforeEach
-    @SuppressWarnings("unchecked")
-    public void setup() {
-        reflections = new Reflections("nextstep.di.factory.example");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
-        beanFactory = new BeanFactory(preInstanticateClazz);
-        beanFactory.initialize();
-    }
-
     @Test
     public void di() {
-        QnaController qnaController = beanFactory.getBean(QnaController.class).get();
+        reflections = new Reflections("nextstep.di.factory.example");
+        Set<Class<?>> preInstantiateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        beanFactory = new BeanFactory(preInstantiateClazz);
+        beanFactory.initialize();
+
+        QnaController qnaController = beanFactory.getBean(QnaController.class);
         assertNotNull(qnaController);
         assertNotNull(qnaController.getQnaService());
 
@@ -50,8 +47,30 @@ public class BeanFactoryTest {
     @Test
     @DisplayName("다른 패키지의 클래스를 찾으려고 할 때")
     void another_package_class() {
-        Optional<BeanScanner> bean = beanFactory.getBean(BeanScanner.class);
-        assertThat(bean).isEqualTo(Optional.empty());
+        reflections = new Reflections("nextstep.di.factory.example");
+        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        beanFactory = new BeanFactory(preInstanticateClazz);
+        beanFactory.initialize();
+
+        assertThrows(BeanNotFoundException.class, () -> beanFactory.getBean(BeanScanner.class));
+    }
+
+    @Test
+    @DisplayName("빈이 아닌 클래스가 빈인 클래스의 파라미터로 있을 때")
+    void parameter_is_no_bean() {
+        reflections = new Reflections("nextstep.di.factory.test");
+        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Service.class);
+        beanFactory = new BeanFactory(preInstanticateClazz);
+        assertThrows(ParameterIsNotBeanException.class, () -> beanFactory.initialize());
+    }
+
+    @Test
+    @DisplayName("Inject가 붙은 두개의 빈이 순환참조일 경우")
+    void beans_is_circular_reference() {
+        reflections = new Reflections("nextstep.di.factory.circularreference");
+        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Service.class);
+        beanFactory = new BeanFactory(preInstanticateClazz);
+        assertThrows(CircularReferenceException.class, () -> beanFactory.initialize());
     }
 
     @SuppressWarnings("unchecked")
