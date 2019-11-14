7dc7cb68539ea65c3c6bd58b1e5a4a7e4276da3a
이영현 <leey9494@icloud.com>
2019-11-14 16:37:20 +0900

    refactor: change ClasspathBeanScanner method logic

--- nextstep-di/src/main/java/nextstep/di/scanner/ClasspathBeanScanner.java
+++ nextstep-di/src/main/java/nextstep/di/scanner/ClasspathBeanScanner.java
@@ -1,7 +1,9 @@
 package nextstep.di.scanner;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import nextstep.di.factory.BeanFactory;
+import nextstep.di.factory.BeanFactoryUtils;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
@@ -10,14 +12,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.Map;
 import java.util.Set;
 
 public class ClasspathBeanScanner {
     private static final Logger log = LoggerFactory.getLogger(ClasspathBeanScanner.class);
+    private static final Class[] COMPONENTS = {Controller.class, Service.class, Repository.class};
+
 
     private BeanFactory beanFactory;
     private Reflections reflections;
-    private Set<Class<?>> preInstanticateClazz;
 
     public ClasspathBeanScanner(BeanFactory beanFactory) {
         this.beanFactory = beanFactory;
@@ -25,17 +30,17 @@ public class ClasspathBeanScanner {
 
     public void doScan(Object... basePackage) {
         reflections = new Reflections(basePackage);
-        initiate();
-        getPreInstanticateClazz();
-    }
-
-    public void getPreInstanticateClazz() {
-        beanFactory.addPreInstanticateClazz(preInstanticateClazz);
+        beanFactory.addPreInstanticateClazz(getBeans());
     }
 
     @SuppressWarnings("unchecked")
-    private void initiate() {
-        this.preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+    private Map<Class<?>, Constructor> getBeans() {
+        Set<Class<?>> typesAnnotatedWith = getTypesAnnotatedWith(COMPONENTS);
+        Map<Class<?>, Constructor> beans = Maps.newHashMap();
+        for (Class<?> clazz : typesAnnotatedWith) {
+            beans.put(clazz, BeanFactoryUtils.getInjectedConstructor(clazz));
+        }
+        return beans;
     }
 
     @SuppressWarnings("unchecked")
