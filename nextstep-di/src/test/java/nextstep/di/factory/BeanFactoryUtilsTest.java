c95526cb6e92d36175894cdef19755a8b336f512
pkch93 <star_final@naver.com>
2019-11-14 15:38:52 +0900

    feat: BeanFactoryUtils.findBeanDefinition 추가

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryUtilsTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryUtilsTest.java
@@ -2,12 +2,16 @@ package nextstep.di.factory;
 
 import nextstep.di.factory.example.controller.QnaController;
 import nextstep.di.factory.example.repository.JdbcQuestionRepository;
+import nextstep.di.factory.example.repository.JdbcUserRepository;
+import nextstep.di.factory.example.repository.UserRepository;
 import nextstep.di.factory.example.service.MyQnaService;
 import nextstep.di.factory.outside.MultipleInjectedService;
+import nextstep.di.factory.outside.OutsideService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -35,5 +39,35 @@ class BeanFactoryUtilsTest {
     @DisplayName("@Inject 애너테이션이 없는 경우에는 null을 반환한다.")
     void getInjectedConstructor_IfInjectedConstructorIsEmpty_returnNull() {
         assertNull(BeanFactoryUtils.getInjectedConstructor(JdbcQuestionRepository.class));
+    }
+
+    @Test
+    @DisplayName("인터페이스에 대한 BeanDefinition을 찾아준다.")
+    void findConcreteDefinitionOfInterface() {
+        BeanDefinitionFactory beanDefinitionFactory = new BeanDefinitionFactory(BeanScanner.scanConfiguration("nextstep.di.factory.example"));
+        Map<Class<?>, BeanDefinition> definitions = beanDefinitionFactory.createBeanDefinition();
+
+        BeanDefinition beanDefinition = BeanFactoryUtils.findBeanDefinition(UserRepository.class, definitions);
+        assertThat(beanDefinition.getType()).isEqualTo(JdbcUserRepository.class);
+    }
+
+    @Test
+    @DisplayName("해당하는 클래스에 대한 BeanDefinition을 찾아준다.")
+    void findConcreteDefinitionOfClass() {
+        BeanDefinitionFactory beanDefinitionFactory = new BeanDefinitionFactory(BeanScanner.scanConfiguration("nextstep.di.factory.example"));
+        Map<Class<?>, BeanDefinition> definitions = beanDefinitionFactory.createBeanDefinition();
+
+        BeanDefinition beanDefinition = BeanFactoryUtils.findBeanDefinition(MyQnaService.class, definitions);
+        assertThat(beanDefinition.getType()).isEqualTo(MyQnaService.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 BeanDefinition을 찾는 경우 예외를 던진다.")
+    void failToFindDefinition() {
+        BeanDefinitionFactory beanDefinitionFactory = new BeanDefinitionFactory(BeanScanner.scanConfiguration("nextstep.di.factory.example"));
+        Map<Class<?>, BeanDefinition> definitions = beanDefinitionFactory.createBeanDefinition();
+
+        assertThatThrownBy(() -> BeanFactoryUtils.findBeanDefinition(OutsideService.class, definitions))
+                .isInstanceOf(IllegalStateException.class);
     }
 }
