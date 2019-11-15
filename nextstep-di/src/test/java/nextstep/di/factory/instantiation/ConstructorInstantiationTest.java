53ce6c9eddd89cdf386bf16954661c0c93c158a1
heebong <heebg2@naver.com>
2019-11-15 12:40:08 +0900

    test: configuration에 있는 항목을 생성자 파라미터로 가지는 `@Repository` 생성

--- nextstep-di/src/test/java/nextstep/di/factory/instantiation/ConstructorInstantiationTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/instantiation/ConstructorInstantiationTest.java
@@ -3,11 +3,17 @@ package nextstep.di.factory.instantiation;
 import com.google.common.collect.Maps;
 import nextstep.di.factory.BeanCreateMatcher;
 import nextstep.di.factory.example.JdbcQuestionRepository;
+import nextstep.di.factory.example.JdbcQuestionWithJdbcRepository;
 import nextstep.di.factory.example.JdbcUserRepository;
+import nextstep.di.factory.example.MyJdbcTemplate;
 import nextstep.di.factory.example.MyQnaService;
-import org.junit.jupiter.api.AfterEach;
+import nextstep.di.factory.example.config.IntegrationConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -26,6 +32,20 @@ class ConstructorInstantiationTest {
     @Test
     void getInstance() {
         constructorInstantiation = new ConstructorInstantiation(MyQnaService.class);
+        assertNotNull(constructorInstantiation.getInstance(beanCreateMatcher, Maps.newHashMap()));
+    }
+
+    @Test
+    void getInstanceWithMethodInstantiation() throws NoSuchMethodException {
+        Class<?> clazz = IntegrationConfig.class;
+        Object clazzInstance = BeanUtils.instantiateClass(clazz);
+        Method dataSource = clazz.getMethod("dataSource");
+        Method jdbcTemplate = clazz.getMethod("jdbcTemplate", DataSource.class);
+        beanCreateMatcher.put(DataSource.class, new MethodInstantiation(dataSource, clazzInstance));
+        beanCreateMatcher.put(MyJdbcTemplate.class, new MethodInstantiation(jdbcTemplate, clazzInstance));
+        beanCreateMatcher.put(JdbcQuestionWithJdbcRepository.class, new ConstructorInstantiation(JdbcQuestionWithJdbcRepository.class));
+
+        constructorInstantiation = new ConstructorInstantiation(JdbcQuestionWithJdbcRepository.class);
         assertNotNull(constructorInstantiation.getInstance(beanCreateMatcher, Maps.newHashMap()));
     }
 }
