da911db20796d5a0cd9e458fc371a8774e23efe1
kangmin46 <kangmin789@naver.com>
2019-11-17 17:22:06 +0900

    refactor : rename BeanScanner to ComponentScanner

---
+++ nextstep-di/src/main/java/nextstep/di/factory/ComponentScanner.java
@@ -0,0 +1,36 @@
+package nextstep.di.factory;
+
+import com.google.common.collect.Sets;
+import nextstep.stereotype.Controller;
+import nextstep.stereotype.Repository;
+import nextstep.stereotype.Service;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+public class ComponentScanner {
+    private static final Logger log = LoggerFactory.getLogger(ComponentScanner.class);
+
+    private Reflections reflections;
+
+    public ComponentScanner(Object... basePackage) {
+        reflections = new Reflections(basePackage);
+    }
+
+    public Set<Class<?>> getPreInstanticateClass() {
+        return getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
+        Set<Class<?>> beans = Sets.newHashSet();
+        for (Class<? extends Annotation> annotation : annotations) {
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        log.debug("Scan Beans Type : {}", beans);
+        return beans;
+    }
+}
