6eb7c58fc9292ffc6b1f1bbecbf12cff82c7e39a
aiden <cnk9109@gmail.com>
2019-11-19 12:09:23 +0900

    feat: ConstructorBeanDefinition 생성

---
+++ nextstep-di/src/main/java/nextstep/di/ConstructorBeanDefinition.java
@@ -0,0 +1,45 @@
+package nextstep.di;
+
+import nextstep.di.exception.CreateBeanException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class ConstructorBeanDefinition implements BeanDefinition {
+    private static final Logger logger = LoggerFactory.getLogger(ConstructorBeanDefinition.class);
+
+    private Class<?> clazz;
+    private Constructor<?> constructor;
+
+    public ConstructorBeanDefinition(Class<?> clazz, Constructor<?> constructor) {
+        this.clazz = clazz;
+        this.constructor = constructor;
+    }
+
+    @Override
+    public Class<?> getType() {
+        return clazz;
+    }
+
+    @Override
+    public Class<?>[] getParameterTypes() {
+        return constructor.getParameterTypes();
+    }
+
+    @Override
+    public boolean isType(Class<?> type) {
+        return getType().equals(type);
+    }
+
+    @Override
+    public Object createBean(Object... params) {
+        try {
+            return constructor.newInstance(params);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            logger.error("{} Bean instance fail. error message : {}", getType(), e.getMessage());
+            throw new CreateBeanException(e);
+        }
+    }
+}
