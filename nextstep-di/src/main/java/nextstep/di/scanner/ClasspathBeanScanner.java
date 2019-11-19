09b7ce5be49b5c90fa434a8dce18ac630f3b1a5d
이영현 <leey9494@icloud.com>
2019-11-19 15:23:14 +0900

    refactor: change method to util method

--- nextstep-di/src/main/java/nextstep/di/scanner/ClasspathBeanScanner.java
+++ nextstep-di/src/main/java/nextstep/di/scanner/ClasspathBeanScanner.java
@@ -1,7 +1,6 @@
 package nextstep.di.scanner;
 
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import nextstep.di.factory.BeanFactory;
 import nextstep.di.factory.BeanFactoryUtils;
 import nextstep.stereotype.Controller;
@@ -11,7 +10,6 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.Set;
@@ -19,7 +17,6 @@ import java.util.Set;
 public class ClasspathBeanScanner {
     private static final Logger log = LoggerFactory.getLogger(ClasspathBeanScanner.class);
     private static final Class[] COMPONENTS = {Controller.class, Service.class, Repository.class};
-
 
     private BeanFactory beanFactory;
     private Reflections reflections;
@@ -30,12 +27,14 @@ public class ClasspathBeanScanner {
 
     @SuppressWarnings("unchecked")
     private Map<Class<?>, Constructor> getBeans() {
-        Set<Class<?>> typesAnnotatedWith = getTypesAnnotatedWith(COMPONENTS);
+        Set<Class<?>> typesAnnotatedWith = ScannerUtils.getTypesAnnotateWith(reflections, COMPONENTS);
         Map<Class<?>, Constructor> beans = Maps.newHashMap();
 
         for (Class<?> clazz : typesAnnotatedWith) {
             beans.put(clazz, BeanFactoryUtils.getInjectedConstructor(clazz));
         }
+
+        log.debug("component bean : {}", beans);
 
         return beans;
     }
@@ -43,15 +42,5 @@ public class ClasspathBeanScanner {
     public void register(Object... basePackage) {
         reflections = new Reflections(basePackage);
         beanFactory.putComponentBean(getBeans());
-    }
-
-    @SuppressWarnings("unchecked")
-    public Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Set<Class<?>> beans = Sets.newHashSet();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        log.debug("Scan Beans Type : {}", beans);
-        return beans;
     }
 }
---
+++ nextstep-di/src/main/java/nextstep/di/scanner/ScannerUtils.java
@@ -0,0 +1,25 @@
+package nextstep.di.scanner;
+
+import com.google.common.collect.Sets;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+public class ScannerUtils {
+    private static final Logger log = LoggerFactory.getLogger(ScannerUtils.class);
+
+    @SuppressWarnings("unchecked")
+    public static Set<Class<?>> getTypesAnnotateWith(Reflections reflections, Class<? extends Annotation>... annotations) {
+        Set<Class<?>> annotationBeans = Sets.newHashSet();
+
+        for (Class<? extends Annotation> annotation : annotations) {
+            annotationBeans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+
+        log.debug("Scan Beans Type : {}", annotationBeans);
+        return annotationBeans;
+    }
+}
