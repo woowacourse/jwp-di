f38ee2bb043091677d1667932037d7b118b0803c
aiden <cnk9109@gmail.com>
2019-11-19 16:41:30 +0900

    feat: initializeRoot() 추가

--- nextstep-di/src/main/java/nextstep/di/context/ApplicationBeanContext.java
+++ nextstep-di/src/main/java/nextstep/di/context/ApplicationBeanContext.java
@@ -1,10 +1,13 @@
 package nextstep.di.context;
 
+import nextstep.annotation.ComponentScan;
 import nextstep.di.factory.BeanFactory;
 import nextstep.di.factory.SingleBeanFactory;
 import nextstep.di.registry.BeanRegistry;
 import nextstep.di.scanner.AnnotatedBeanScanner;
 import nextstep.di.scanner.ConfigurationScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.util.Collections;
@@ -12,13 +15,33 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ApplicationBeanContext implements BeanFactory {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationBeanContext.class);
 
     private BeanRegistry beanRegistry = new BeanRegistry();
     private Object[] root;
 
     public ApplicationBeanContext(Object... root) {
-        this.root = root;
+        this.root = initializeRoot(root);
         initialize();
+    }
+
+    private Object[] initializeRoot(Object... root) {
+        for (Object o : root) {
+            if (o instanceof Class) {
+                Object[] clazz = getComponentScan((Class) o);
+                if (clazz != null) return clazz;
+            }
+        }
+
+        return root;
+    }
+
+    private Object[] getComponentScan(Class o) {
+        Class<?> clazz = o;
+        if (clazz.isAnnotationPresent(ComponentScan.class)) {
+            return clazz.getAnnotation(ComponentScan.class).value();
+        }
+        return null;
     }
 
     @Override
