7daf6bbe88e1f349c9450764ff8147ce865951fc
이영현 <leey9494@icloud.com>
2019-11-19 15:05:29 +0900

    style: add configuration bean

--- nextstep-mvc/src/test/java/samples/MyConfiguration.java
+++ nextstep-mvc/src/test/java/samples/MyConfiguration.java
@@ -1,7 +1,13 @@
 package samples;
 
+import nextstep.annotation.Bean;
 import nextstep.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
+
+    @Bean
+    public Car car() {
+        return new Car("red", "suv");
+    }
 }
