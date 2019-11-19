eb24c9b8a19687297da670c3e309f0337061c4e9
aiden <cnk9109@gmail.com>
2019-11-19 11:57:21 +0900

    feat: BeanDefinitionException 생성

---
+++ nextstep-di/src/main/java/nextstep/di/exception/BeanDefinitionException.java
@@ -0,0 +1,7 @@
+package nextstep.di.exception;
+
+public class BeanDefinitionException extends RuntimeException {
+    public BeanDefinitionException(Throwable cause) {
+        super(cause);
+    }
+}
