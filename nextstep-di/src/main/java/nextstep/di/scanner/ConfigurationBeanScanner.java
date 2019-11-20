b1e740f180081d448f66a6387877859996364721
mrkwon <erased11@gmail.com>
2019-11-20 14:01:25 +0900

    refactor: add logger and remove not-used methods

--- nextstep-di/src/main/java/nextstep/di/scanner/ConfigurationBeanScanner.java
+++ nextstep-di/src/main/java/nextstep/di/scanner/ConfigurationBeanScanner.java
@@ -1,23 +1,18 @@
 package nextstep.di.scanner;
 
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import nextstep.annotation.Bean;
 import nextstep.annotation.Configuration;
 import nextstep.di.factory.BeanFactory;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Map;
-import java.util.Set;
 
 public class ConfigurationBeanScanner {
     private static final Logger log = LoggerFactory.getLogger(ClasspathBeanScanner.class);
 
-    private Reflections reflections;
     private BeanFactory beanFactory;
 
     public ConfigurationBeanScanner(BeanFactory beanFactory) {
@@ -34,25 +29,12 @@ public class ConfigurationBeanScanner {
         Method[] methods = clazz.getMethods();
         for (Method method : methods) {
             if (method.isAnnotationPresent(Bean.class)) {
-                configs.put(method.getReturnType(), method);
+                Class<?> returnType = method.getReturnType();
+                configs.put(returnType, method);
+                log.debug("return type : {}, method : {}", returnType, configs.get(returnType));
             }
         }
 
         beanFactory.registerBean(configs);
-    }
-
-    @SuppressWarnings("unchecked")
-    public Set<Class<?>> getBeans() {
-        return getTypesAnnotatedWith(Configuration.class);
-    }
-
-    @SuppressWarnings("unchecked")
-    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Set<Class<?>> beans = Sets.newHashSet();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        log.debug("Scan Beans Type : {}", beans);
-        return beans;
     }
 }
