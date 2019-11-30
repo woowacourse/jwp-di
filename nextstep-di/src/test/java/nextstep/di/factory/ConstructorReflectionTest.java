121671b43d8b5d688d3044ba60564028c0ff9acf
jinwook <jin942002@naver.com>
2019-12-01 04:24:19 +0900

    refactor: delete incomplete test

--- nextstep-di/src/test/java/nextstep/di/factory/ConstructorReflectionTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/ConstructorReflectionTest.java
@@ -1,11 +1,7 @@
 package nextstep.di.factory;
 
-import nextstep.annotation.Bean;
 import nextstep.di.factory.example.repository.JdbcUserRepository;
-import nextstep.di.factory.example.service.TestService;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,17 +10,6 @@ public class ConstructorReflectionTest {
     @Test
     void checkEmptyConstructorTest() {
         assertThat(JdbcUserRepository.class.getDeclaredConstructors().length).isEqualTo(1);
-    }
-
-    @Test
-    void method_bean() {
-        Class<?> clazz = TestService.class;
-        for (Method declaredMethod : clazz.getDeclaredMethods()) {
-            if (declaredMethod.isAnnotationPresent(Bean.class)) {
-
-            }
-        }
-
     }
 
 }
