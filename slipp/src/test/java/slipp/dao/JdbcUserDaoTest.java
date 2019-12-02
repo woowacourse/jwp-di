8e31a5045d63d13b1bf9024c92ed36e29404b36c
ep1stas1s <masloz@konkuk.ac.kr>
2019-12-03 03:58:22 +0900

    feat: apply configuration annotation

--- slipp/src/test/java/slipp/dao/JdbcUserDaoTest.java
+++ slipp/src/test/java/slipp/dao/JdbcUserDaoTest.java
@@ -1,6 +1,7 @@
 package slipp.dao;
 
 import nextstep.jdbc.ConnectionManager;
+import nextstep.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -14,6 +15,9 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JdbcUserDaoTest {
+
+    private UserDao userDao;
+
     @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
@@ -24,20 +28,20 @@ public class JdbcUserDaoTest {
     @Test
     public void crud() throws Exception {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
-        JdbcUserDao jdbcUserDao = new JdbcUserDao();
-        jdbcUserDao.insert(expected);
-        User actual = jdbcUserDao.findByUserId(expected.getUserId());
+        userDao = new JdbcUserDao(new JdbcTemplate(ConnectionManager.getDataSource()));
+        userDao.insert(expected);
+        User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
 
         expected.update(new UserUpdatedDto("password2", "name2", "sanjigi@email.com"));
-        jdbcUserDao.update(expected);
-        actual = jdbcUserDao.findByUserId(expected.getUserId());
+        userDao.update(expected);
+        actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void findAll() throws Exception {
-        JdbcUserDao jdbcUserDao = new JdbcUserDao();
+        JdbcUserDao jdbcUserDao = new JdbcUserDao(new JdbcTemplate(ConnectionManager.getDataSource()));
         List<User> users = jdbcUserDao.findAll();
         assertThat(users).hasSize(1);
     }
