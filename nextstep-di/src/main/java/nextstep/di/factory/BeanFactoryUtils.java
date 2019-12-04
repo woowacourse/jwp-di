337c6425c24071021791d15a477bcb9c1e7503d2
yeji <yjll1019@naver.com>
2019-12-05 01:14:48 +0900

    feat: add instantiateClass to use when creating a bean with the method

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactoryUtils.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactoryUtils.java
@@ -1,6 +1,8 @@
 package nextstep.di.factory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
@@ -47,5 +49,14 @@ public class BeanFactoryUtils {
         }
 
         throw new IllegalStateException(injectedClazz + "인터페이스를 구현하는 Bean이 존재하지 않는다.");
+    }
+
+    public static Object instantiateClass(Method method) {
+        final Class<?> clazz = method.getDeclaringClass();
+        try {
+            return method.invoke(clazz.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
