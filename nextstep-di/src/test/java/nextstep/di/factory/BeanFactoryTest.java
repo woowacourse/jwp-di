2920f98781472df9c0a41173103e155646489b32
kangmin46 <kangmin789@naver.com>
2019-12-06 22:09:39 +0900

    refactor : modify BeanFactoryTest

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryTest.java
@@ -1,6 +1,9 @@
 package nextstep.di.factory;
 
+
 import com.google.common.collect.Sets;
+import nextstep.di.factory.example.IntegrationConfig;
+import nextstep.di.factory.example.MyJdbcTemplate;
 import nextstep.di.factory.example.MyQnaService;
 import nextstep.di.factory.example.QnaController;
 import nextstep.exception.BeanNotFoundException;
@@ -9,13 +12,16 @@ import nextstep.exception.ParameterIsNotBeanException;
 import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.sql.DataSource;
 import java.lang.annotation.Annotation;
+import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,61 +31,25 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BeanFactoryTest {
     private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
 
-    private Reflections reflections;
-    private BeanFactory beanFactory;
+    protected Reflections reflections;
+    protected BeanFactory beanFactory;
 
-    @Test
-    public void di() {
-        reflections = new Reflections("nextstep.di.factory.example");
-        Set<Class<?>> preInstantiateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
-        beanFactory = new BeanFactory(preInstantiateClazz);
-        beanFactory.initialize();
+    @BeforeEach
+    void setUp() {
+        BeanScanner beanScanner = new BeanScanner("nextstep.di.factory.example");
+        Set<Class<?>> preInstantiateComponents = beanScanner.scan();
+        ConfigurationBeanScanner configurationBeanScanner = new ConfigurationBeanScanner();
+        Set<Class<?>> preInstantiateConfigurationBeans = configurationBeanScanner.scan(IntegrationConfig.class);
 
-        QnaController qnaController = beanFactory.getBean(QnaController.class);
-        assertNotNull(qnaController);
-        assertNotNull(qnaController.getQnaService());
-
-        MyQnaService qnaService = qnaController.getQnaService();
-        assertNotNull(qnaService.getUserRepository());
-        assertNotNull(qnaService.getQuestionRepository());
+        beanFactory = new BeanFactory(preInstantiateComponents, preInstantiateConfigurationBeans);
     }
 
     @Test
-    @DisplayName("다른 패키지의 클래스를 찾으려고 할 때")
-    void another_package_class() {
-        reflections = new Reflections("nextstep.di.factory.example");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
-        beanFactory = new BeanFactory(preInstanticateClazz);
-        beanFactory.initialize();
-
-        assertThrows(BeanNotFoundException.class, () -> beanFactory.getBean(BeanScanner.class));
-    }
-
-    @Test
-    @DisplayName("빈이 아닌 클래스가 빈인 클래스의 파라미터로 있을 때")
-    void parameter_is_no_bean() {
-        reflections = new Reflections("nextstep.di.factory.test");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Service.class);
-        beanFactory = new BeanFactory(preInstanticateClazz);
-        assertThrows(ParameterIsNotBeanException.class, () -> beanFactory.initialize());
-    }
-
-    @Test
-    @DisplayName("Inject가 붙은 두개의 빈이 순환참조일 경우")
-    void beans_is_circular_reference() {
-        reflections = new Reflections("nextstep.di.factory.circularreference");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Service.class);
-        beanFactory = new BeanFactory(preInstanticateClazz);
-        assertThrows(CircularReferenceException.class, () -> beanFactory.initialize());
-    }
-
-    @SuppressWarnings("unchecked")
-    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
-        Set<Class<?>> beans = Sets.newHashSet();
-        for (Class<? extends Annotation> annotation : annotations) {
-            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
-        }
-        log.debug("Scan Beans Type : {}", beans);
-        return beans;
+    void initialize() {
+        Map<Class<?>, Object> beans = beanFactory.initialize(IntegrationConfig.class);
+        assertNotNull(beans.get(MyJdbcTemplate.class));
+        assertNotNull(beans.get(DataSource.class));
+        assertNotNull(beans.get(MyQnaService.class));
+        assertNotNull(beans.get(QnaController.class));
     }
 }
