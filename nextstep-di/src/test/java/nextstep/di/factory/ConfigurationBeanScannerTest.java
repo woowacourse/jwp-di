d18902e3327173c0097827c7d2d8f0f2e688ebb0
kangmin46 <kangmin789@naver.com>
2019-12-06 22:02:23 +0900

    feat : implement ConfigurationBeanScannerTest

---
+++ nextstep-di/src/test/java/nextstep/di/factory/ConfigurationBeanScannerTest.java
@@ -0,0 +1,30 @@
+package nextstep.di.factory;
+
+import nextstep.di.factory.example.IntegrationConfig;
+import nextstep.di.factory.example.MyJdbcTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConfigurationBeanScannerTest {
+    private ConfigurationBeanScanner configurationBeanScanner;
+
+    @BeforeEach
+    void setUp() {
+        configurationBeanScanner = new ConfigurationBeanScanner();
+    }
+
+    @Test
+    @DisplayName("설정파일의 Bean 메소드를 찾아서 등록")
+    void register() {
+        Set<Class<?>> classes = configurationBeanScanner.scan(IntegrationConfig.class);
+        assertTrue(classes.contains(DataSource.class));
+        assertTrue(classes.contains(MyJdbcTemplate.class));
+    }
+}
