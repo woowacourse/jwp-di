98f9d8fa0499d7833d669dc0d2e3d7eeef5a4391
kangmin46 <kangmin789@naver.com>
2019-12-06 22:07:23 +0900

    feat : change structure application context

--- nextstep-di/src/main/java/nextstep/di/factory/ApplicationContext.java
+++ nextstep-di/src/main/java/nextstep/di/factory/ApplicationContext.java
@@ -4,10 +4,12 @@ import nextstep.annotation.ComponentScan;
 import nextstep.exception.ComponentScanFindFailException;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ApplicationContext {
     private final Class<?> configureClass;
+    private BeanFactory beanFactory;
 
     public ApplicationContext(final Class<?> configureClass) {
         this.configureClass = configureClass;
@@ -15,11 +17,25 @@ public class ApplicationContext {
 
     public Set<Class<?>> initialize() {
         String[] basePackage = findBasePackage();
+
         BeanScanner beanScanner = new BeanScanner(basePackage);
-        Set<Class<?>> preInstantiateBeans = beanScanner.scan();
-        ConfigurationBeanScanner configurationBeanScanner = new ConfigurationBeanScanner(basePackage);
-        Set<Class<?>> preInstantiateConfigurationBeans = configurationBeanScanner.scan();
-        return unify(preInstantiateBeans, preInstantiateConfigurationBeans);
+        Set<Class<?>> preInstantiateComponents = beanScanner.scan();
+
+        ConfigurationBeanScanner configurationBeanScanner = new ConfigurationBeanScanner();
+
+        Set<Class<?>> preInstantiateConfigurationBeans = configurationBeanScanner.scan(configureClass);
+
+        this.beanFactory = new BeanFactory(preInstantiateComponents, preInstantiateConfigurationBeans);
+        beanFactory.initialize(configureClass);
+        return null;
+    }
+
+    private String[] findBasePackage() {
+        if(configureClass.isAnnotationPresent(ComponentScan.class)) {
+            ComponentScan componentScan = configureClass.getAnnotation(ComponentScan.class);
+            return componentScan.basePackages();
+        }
+        throw new ComponentScanFindFailException();
     }
 
     private Set<Class<?>> unify(Set<Class<?>> preInstantiateBeans, Set<Class<?>> preInstantiateConfigurationBeans) {
@@ -29,11 +45,7 @@ public class ApplicationContext {
         return preInstantiateClazz;
     }
 
-    private String[] findBasePackage() {
-        if(configureClass.isAnnotationPresent(ComponentScan.class)) {
-            ComponentScan componentScan = configureClass.getAnnotation(ComponentScan.class);
-            return componentScan.basePackages();
-        }
-        throw new ComponentScanFindFailException();
+    public <T> T getBean(Class<T> requiredType) {
+        return this.beanFactory.getBean(requiredType);
     }
 }
