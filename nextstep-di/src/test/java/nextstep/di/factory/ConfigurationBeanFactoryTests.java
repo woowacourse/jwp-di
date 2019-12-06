79ed1ccbbd57511a4f31367247f95f8d4fb8b5f9
kangmin46 <kangmin789@naver.com>
2019-12-06 22:26:24 +0900

    feat : add ConfigurationBeanFactoryTest case

--- nextstep-di/src/test/java/nextstep/di/factory/ConfigurationBeanFactoryTests.java
+++ nextstep-di/src/test/java/nextstep/di/factory/ConfigurationBeanFactoryTests.java
@@ -1,22 +1,16 @@
 package nextstep.di.factory;
 
-import nextstep.annotation.Configuration;
-import nextstep.di.factory.example.ExampleConfig;
 import nextstep.di.factory.example.IntegrationConfig;
 import nextstep.di.factory.example.MyJdbcTemplate;
-import nextstep.di.factory.example.MyQnaService;
-import nextstep.di.factory.example.QnaController;
-import nextstep.stereotype.Controller;
-import nextstep.stereotype.Repository;
-import nextstep.stereotype.Service;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
 
 import javax.sql.DataSource;
 import java.util.Map;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ConfigurationBeanFactoryTests {
@@ -42,5 +36,13 @@ public class ConfigurationBeanFactoryTests {
 
         assertNotNull(configurationBeans.get(MyJdbcTemplate.class));
         assertNotNull(configurationBeans.get(DataSource.class));
+    }
+
+    @Test
+    @DisplayName("Config 클래스에서 component의 DI가 가능한지 테스트")
+    void component_configuration_di() {
+        ComponentFactory componentFactory = new ComponentFactory(beanDefinition);
+        Map<Class<?>, Object> components = componentFactory.initialize();
+        assertDoesNotThrow(() -> configurationBeanFactory.initialize(components));
     }
 }
--- nextstep-di/src/test/java/nextstep/di/factory/example/ExampleConfig.java
+++ nextstep-di/src/test/java/nextstep/di/factory/example/ExampleConfig.java
@@ -1,20 +1,17 @@
 package nextstep.di.factory.example;
 
 import nextstep.annotation.Bean;
+import nextstep.annotation.ComponentScan;
 import nextstep.annotation.Configuration;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan(basePackages = "nextstep.di.factory.example")
 public class ExampleConfig {
     @Bean
-    public DataSource dataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("org.h2.Driver");
-        ds.setUrl("jdbc:h2:~/jwp-framework;MVCC=TRUE;DB_CLOSE_ON_EXIT=FALSE");
-        ds.setUsername("sa");
-        ds.setPassword("");
-        return ds;
+    public DataSource dataSource(MyQnaService myQnaService) {
+        return new BasicDataSource();
     }
 }
