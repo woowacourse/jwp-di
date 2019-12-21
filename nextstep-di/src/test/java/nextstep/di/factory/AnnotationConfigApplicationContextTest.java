88adc745184d6c29fead2e744525368db4ffe609
green4469 <green4469@gmail.com>
2019-12-21 17:39:14 +0900

    test: add AnnotationConfigApplicationContextTest

---
+++ nextstep-di/src/test/java/nextstep/di/factory/AnnotationConfigApplicationContextTest.java
@@ -0,0 +1,47 @@
+package nextstep.di.factory;
+
+import nextstep.di.factory.example.*;
+import nextstep.di.factory.example.BeanInjectedComponent;
+import nextstep.di.factory.example.component.MyJdbcTemplate;
+import nextstep.di.factory.example.component.QnaController;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AnnotationConfigApplicationContextTest {
+
+    @Test
+    public void integrationConfigTest() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(IntegrationConfig.class);
+
+        DataSource dataSource = (DataSource) context.getBean(DataSource.class);
+        assertNotNull(dataSource);
+
+        MyJdbcTemplate jdbcTemplate = (MyJdbcTemplate) context.getBean(MyJdbcTemplate.class);
+        assertNotNull(jdbcTemplate);
+        assertNotNull(jdbcTemplate.getDataSource());
+
+        assertThat(jdbcTemplate.getDataSource()).isEqualTo(dataSource);
+    }
+
+    @Test
+    public void componentToBeanWiringTest() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+
+        QnaController qnaController = (QnaController) context.getBean(QnaController.class);
+        assertNotNull(qnaController);
+    }
+
+    @Test
+    public void beanToComponentWiringTest() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+
+        BeanInjectedComponent component = (BeanInjectedComponent) context.getBean(BeanInjectedComponent.class);
+        assertNotNull(component);
+        assertNotNull(component.getDataSource());
+        assertThat(component.getDataSource()).isEqualTo(context.getBean(DataSource.class));
+    }
+}
