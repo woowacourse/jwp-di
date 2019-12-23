22bab97204d139b33bf9af56c1083e5c5c3a0201
이두호 <ddu0422@naver.com>
2019-12-24 01:04:30 +0900

    feat: 불필요한 logger 제거

--- nextstep-di/src/main/java/nextstep/di/factory/ClasspathBeanScanner.java
+++ nextstep-di/src/main/java/nextstep/di/factory/ClasspathBeanScanner.java
@@ -5,15 +5,11 @@ import nextstep.stereotype.Controller;
 import nextstep.stereotype.Repository;
 import nextstep.stereotype.Service;
 import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.util.Set;
 
 public class ClasspathBeanScanner {
-    private static final Logger log = LoggerFactory.getLogger(ClasspathBeanScanner.class);
-
     private BeanFactory beanFactory;
     private Reflections reflections;
     private Set<Class<? extends Annotation>> annotations = Sets.newHashSet();
@@ -45,7 +41,6 @@ public class ClasspathBeanScanner {
         for (Class<? extends Annotation> annotation : annotations) {
             beans.addAll(reflections.getTypesAnnotatedWith(annotation));
         }
-        log.debug("Scan Beans Type : {}", beans);
 
         return beans;
     }
