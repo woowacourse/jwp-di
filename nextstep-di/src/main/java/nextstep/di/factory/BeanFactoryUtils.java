02848b3bb46f1cc0a5200f5a5b070413bbba6075
kangmin46 <kangmin789@naver.com>
2019-12-23 14:03:10 +0900

    refactor : modify BeanFactoryUtils class

--- nextstep-di/src/main/java/nextstep/di/factory/BeanFactoryUtils.java
+++ nextstep-di/src/main/java/nextstep/di/factory/BeanFactoryUtils.java
@@ -35,18 +35,18 @@ public class BeanFactoryUtils {
      * @param preInstantiateBeans
      * @return
      */
-    public static Class<?> findConcreteClass(Class<?> injectedClazz, Set<Class<?>> preInstantiateBeans) {
+    public static Optional<Class<?>> findConcreteClass(Class<?> injectedClazz, Set<Class<?>> preInstantiateBeans) {
         if (!injectedClazz.isInterface()) {
-            return injectedClazz;
+            return Optional.of(injectedClazz);
         }
 
         for (Class<?> clazz : preInstantiateBeans) {
             Set<Class<?>> interfaces = Sets.newHashSet(clazz.getInterfaces());
             if (interfaces.contains(injectedClazz)) {
-                return clazz;
+                return Optional.of(clazz);
             }
         }
 
-        throw new IllegalStateException(injectedClazz + "인터페이스를 구현하는 Bean이 존재하지 않는다.");
+        return Optional.empty();
     }
 }
