e9e4fd7a87e42baec5bebb009f2bf4d8fe85aeee
Ko eunsuk <eunsukko.128@gmail.com>
2019-12-23 14:06:09 +0900

    refactor: BeanFactoryUtils 테스트 추가

--- nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryUtilsTest.java
+++ nextstep-di/src/test/java/nextstep/di/factory/BeanFactoryUtilsTest.java
@@ -1,9 +1,14 @@
 package nextstep.di.factory;
 
+import nextstep.annotation.Inject;
 import nextstep.di.exception.BeanWithoutConstructorException;
+import nextstep.di.factory.example.QnaController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BeanFactoryUtilsTest {
@@ -16,5 +21,13 @@ class BeanFactoryUtilsTest {
     }
 
     class WithoutConstructor {
+    }
+
+    @Test
+    @DisplayName("@Inject 가 달린 생성자가 존재하는 경우")
+    void getBeanConstructor_hasInjectedConstructor () {
+        Constructor<?> constructor = BeanFactoryUtils.getBeanConstructor(QnaController.class);
+
+        assertThat(constructor.isAnnotationPresent(Inject.class)).isTrue();
     }
 }
