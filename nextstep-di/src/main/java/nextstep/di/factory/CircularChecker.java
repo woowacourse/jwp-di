a58b52d4ac569742c4b012d496e46e8e5734a897
kangmin46 <kangmin789@naver.com>
2019-12-23 14:02:49 +0900

    feat : implement CircularChecker class

--- nextstep-di/src/main/java/nextstep/di/factory/CircularChecker.java
+++ nextstep-di/src/main/java/nextstep/di/factory/CircularChecker.java
@@ -1,69 +1,25 @@
 package nextstep.di.factory;
 
-import com.google.common.collect.Lists;
 import nextstep.exception.CircularReferenceException;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Parameter;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.Stack;
 
 public class CircularChecker {
-    private static final int NO_PARAMETER = 0;
-    private final Set<Class<?>> preInstantiateBeans;
+    private Stack<Class<?>> referencedClasses = new Stack<>();
 
-    public CircularChecker(final Set<Class<?>> preInstantiateBeans) {
-        this.preInstantiateBeans = preInstantiateBeans;
-    }
-
-    public void check(final Class<?> clazz) {
-        BeanFactoryUtils.getInjectedConstructor(clazz)
-            .ifPresent(constructor -> checkComponentCircularReference(constructor, Lists.newArrayList()));
-    }
-
-    private void checkComponentCircularReference(final Constructor<?> injectedConstructor, List<Class<?>> ownerClasses) {
-        Parameter[] parameters = injectedConstructor.getParameters();
-
-        if (parameters.length == NO_PARAMETER) {
-            return;
+    public void check(Class<?> param) {
+        if (isReferenced(param)) {
+            throw new CircularReferenceException();
         }
 
-        if (ownerClasses.isEmpty()) {
-            Class<?> ownerClass = injectedConstructor.getDeclaringClass();
-            ownerClasses.add(ownerClass);
-            checkComponentCircularReference(parameters, ownerClasses);
-            return;
-        }
-
-        Parameter[] injectedParameters = injectedConstructor.getParameters();
-        checkCircularReference(injectedParameters, ownerClasses);
-
-        Class<?> slaveClass = injectedConstructor.getDeclaringClass();
-        ownerClasses.add(slaveClass);
-        checkComponentCircularReference(injectedParameters, ownerClasses);
+        referencedClasses.push(param);
     }
 
-    private void checkComponentCircularReference(final Parameter[] parameters, List<Class<?>> ownerClasses) {
-        for (Parameter parameter : parameters) {
-            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameter.getType(), preInstantiateBeans);
-            Optional<Constructor<?>> maybeInjectedParameterConstructor = BeanFactoryUtils.getInjectedConstructor(concreteClass);
-            maybeInjectedParameterConstructor.ifPresent(inject -> checkComponentCircularReference(inject, ownerClasses));
-        }
+    private boolean isReferenced(Class<?> param) {
+        return referencedClasses.search(param) >= 0;
     }
 
-    private void checkCircularReference(final Parameter[] injectedParameters, List<Class<?>> ownerClasses) {
-        for (Parameter injectedParameter : injectedParameters) {
-            Class<?> type = injectedParameter.getType();
-            if (ownerClasses.contains(type)) {
-                throw new CircularReferenceException();
-            }
-        }
-
-//        Arrays.stream(injectedParameters)
-//            .map(Parameter::getType)
-//            .filter(ownerClasses::contains)
-//            .findAny()
-//            .orElseThrow(CircularReferenceException::new);
+    public void remove() {
+        referencedClasses.pop();
     }
 }
