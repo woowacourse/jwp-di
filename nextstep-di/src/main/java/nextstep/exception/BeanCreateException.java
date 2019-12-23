77146e514c3946a72ae3697e79fe8ef8704bb87c
kangmin46 <kangmin789@naver.com>
2019-12-23 14:02:28 +0900

    feat : implment BeanCreateException, ConfigurationCreateException class

---
+++ nextstep-di/src/main/java/nextstep/exception/BeanCreateException.java
@@ -0,0 +1,7 @@
+package nextstep.exception;
+
+public class BeanCreateException extends RuntimeException {
+    public BeanCreateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
