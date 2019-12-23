7ad82eecf037bbbcbc187133f6ec391013bbabf3
kangmin46 <kangmin789@naver.com>
2019-12-23 14:00:13 +0900

    feat : implement ConfigurationDefinition class

---
+++ nextstep-di/src/main/java/nextstep/di/factory/definition/ConfigurationDefinition.java
@@ -0,0 +1,36 @@
+package nextstep.di.factory.definition;
+
+import nextstep.exception.BeanCreateException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class ConfigurationDefinition implements BeanDefinition {
+    private Method beanMethod;
+    private Object instance;
+
+    public ConfigurationDefinition(Method beanMethod, Object instance) {
+        this.beanMethod = beanMethod;
+        this.instance = instance;
+    }
+
+    @Override
+    public Object generateBean(Object... params) {
+        try {
+            return beanMethod.invoke(instance, params);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new BeanCreateException();
+        }
+    }
+
+    @Override
+    public Class<?> getType() {
+        return this.beanMethod.getReturnType();
+    }
+
+    @Override
+    public Class<?>[] getParams() {
+        return beanMethod.getParameterTypes();
+    }
+}
