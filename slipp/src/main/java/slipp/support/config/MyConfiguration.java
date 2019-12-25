216e2b38f3340b6ba2ad8a22bb4be4f402cee4f3
CODEMCD <psbum1609@gmail.com>
2019-12-25 23:15:17 +0900

    feat: MyConfiguration 추가

---
+++ slipp/src/main/java/slipp/support/config/MyConfiguration.java
@@ -0,0 +1,23 @@
+package slipp.support.config;
+
+import nextstep.annotation.Bean;
+import nextstep.annotation.ComponentScan;
+import nextstep.annotation.Configuration;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan({ "next", "core" })
+public class MyConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setUrl("jdbc:h2:~/jwp-framework;MVCC=TRUE;DB_CLOSE_ON_EXIT=FALSE");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
+}
