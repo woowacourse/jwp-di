13017f1b86f45fccd6b63ec2cc6ed150fbb315e1
gch01410 <gch01410@naver.com>
2020-01-01 15:47:42 +0900

    refactor: ComponentScan 어노테이션이 있는지 확인하는 기능 삭제

--- nextstep-di/src/main/java/nextstep/di/scanner/ClassPathScanner.java
+++ nextstep-di/src/main/java/nextstep/di/scanner/ClassPathScanner.java
@@ -14,23 +14,11 @@ public class ClassPathScanner {
     private Set<String> packages;
 
     public ClassPathScanner(Class<?>... configClasses) {
-        checkComponentAnnotation(configClasses);
         this.packages = Stream.of(configClasses)
                 .filter(clazz -> clazz.isAnnotationPresent(ANNOTATION_SCAN_CLASS))
                 .map(clazz -> clazz.getAnnotation(ANNOTATION_SCAN_CLASS))
                 .flatMap(annotation -> Stream.of(annotation.basePackages()))
                 .collect(toSet());
-    }
-
-    private void checkComponentAnnotation(Class<?>[] configClasses) {
-        if (!hasComponentScanAnnotation(configClasses)) {
-            throw new RuntimeException("ComponentScan annotation is NOT exist");
-        }
-    }
-
-    private boolean hasComponentScanAnnotation(Class[] configClasses) {
-        return Stream.of(configClasses)
-                .anyMatch(clazz -> clazz.isAnnotationPresent(ANNOTATION_SCAN_CLASS));
     }
 
     public Set<String> getPackages() {
